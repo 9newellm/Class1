750eea0a4d5ca22907cd5ab9ffd2d83d69aa7e17
Your Name <you@example.com>
2025-04-08 08:00:00 -0700

    Format_devloping_TheorticalComaprisons

--- TheorticalCalculations/FormatConfig.py
+++ TheorticalCalculations/FormatConfig.py
@@ -1,1 +1,8 @@
+""" Configurable Formating Tools"""
+
 LINE_UNDERSCORE_HOLD_MAIN_15 = "________________________________________________________________________"
+
+
+""" Configuration Fucntions <Not finalized>"""
+def Call_LINE_UNDERSCORE_HOLD_MAIN_15(): 
+    return print(LINE_UNDERSCORE_HOLD_MAIN_15)
